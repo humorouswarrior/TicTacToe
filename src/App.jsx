f5e517b78252c62639bc72753d359e156bac289a
Vaibhav <khandelwalv971@gmail.com>
2022-06-03 00:49:57 +0530

    initial commit to the project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default () => (
+  <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </>
+);
+
+//may also be written as
+
+// const app = () => {
+//   return (
+//     <>
+//     <h1>Welcome to React Vite Micro App!</h1>
+//     <p>Hard to get more minimal than this React app.</p>
+//   </>
+//   );
+// };
+
+// export default app;
